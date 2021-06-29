8b2b3c34ee1ae06a4c03afcc8e1d58a2d6c3fa0e
komal.malani <komal.malani@HDC1-L-4GXMZY2.dir.svc.accenture.com>
2021-06-29 12:19:42 +0530

    Driver changes

--- src/test/java/batchRunner/TestBatchRun.java
+++ src/test/java/batchRunner/TestBatchRun.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber .class)
+@RunWith(Cucumber.class)
 @CucumberOptions(features="cucumberfile.feature",glue="cucumberstepdef",
 tags= {"@Regression"},plugin="json:target/cucumber-report.json")
 public class TestBatchRun {
